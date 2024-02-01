2a21381881241d223e0b254e9cfa13f562635293
Rebelstarprabhas <155239620+Rebelstarprabhas@users.noreply.github.com>
2024-02-01 12:53:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/UnknownBotz/AutoFilterWithStream.git /AutoFilterWithStream
+  git clone https://github.com/MwsFilesbot/AutoFilterWithStream.git /AutoFilterWithStream
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterWithStream
